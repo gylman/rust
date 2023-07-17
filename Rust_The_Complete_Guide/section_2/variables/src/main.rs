46de019f5ef5ff0b3cd97e8a5e959eb91bd89e64
gylman <gylmanbm@gmail.com>
2023-07-17 13:24:53 +0900

    variables

--- Rust_The_Complete_Guide/section_2/variables/src/main.rs
+++ Rust_The_Complete_Guide/section_2/variables/src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
     let mut x = 5;
-
-    println!("The value of x is {}", x);
-
+    println!("The value is x is {}", x);
     x = 6;
-    println!("The value of x is {}", x);
-
+    println!("The value is x is {}", x);
     const SECONDS: i8 = 60;
     println!("The value of seconds is {}", SECONDS);
 }
