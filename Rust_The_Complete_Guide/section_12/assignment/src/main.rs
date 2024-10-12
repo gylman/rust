db006c529bcdb2e8e543ad1c80027745c3c7054e
gylman <gylmanbm@gmail.com>
2024-10-13 03:05:01 +0900

    chore: remove previous files

--- Rust_The_Complete_Guide/section_12/assignment/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let vec = vec![1,3,5,7,9];
-    let result: Vec<i16> = vec.iter().map(|x| x * 10).collect();
-    println!("{:?}", vec);
-    println!("{:?}", result);
-}
--- Rust_The_Complete_Guide/section_2/variables/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut x = 5;
-    println!("The value is x is {}", x);
-    x = 6;
-    println!("The value is x is {}", x);
-    const SECONDS: i8 = 60;
-    println!("The value of seconds is {}", SECONDS);
-}
